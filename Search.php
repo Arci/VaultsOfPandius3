a7df2673cba87bed3607df838b7abb743521ef8c
Mat Beg <beg@hotmail.it>
2012-08-07 09:14:35 +0200

    screen adj search.php

--- Search.php
+++ Search.php
@@ -6,45 +6,36 @@ http://www.w3.org/TR/html4/loose.dtd>
         <title>Search</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" href="Style/base.css"/>
-
+		<link rel="stylesheet" href="Style/contenent.css"/>
+        <link rel="stylesheet" href="Style/search.css"/>
+		
         <script type="text/javascript" src="Script/zxml.js"></script>
         <script type="text/javascript" src="Script/Xml.js"></script>
         <script type="text/javascript" src="Script/Login.js"></script>
         <script type="text/javascript" src="Script/Post.js"></script>
-        <style>
-            #result{
-                padding: 20px;
-                overflow:auto;
-                max-height: 200px;
-            }
-        </style>
-
         <script type="text/javascript" src="Script/sections.js"></script>
     </head>
     <body>       
 
         <?php include 'Banner.php' ?>
-        <div id="central">    
-            <div>
-                <center>
-                    <form>
-                        <label for="field">Insert text</label><input id="field" type="text">
-                        <input id="button" type="button" value="Search" onclick="search(field.value)">
-                    </form>
-                </center>
-            </div>
-            <div id="result" class="shadowbox" style="visibility: hidden ">
-                
-            </div>
-            <div id="divContent" class="shadowbox" style="visibility: hidden">	    
-                <h2 id=title >Content</h2>
-                <div class="hr"></div>
-                <div id=author></div>
-                <div id=date></div>
-                <div class="hr"></div>
-                <div id=content ></div>
-            </div>
-        </div>
+			<div id="selected">
+				<form>
+					<input id="field" type="text">
+					<input id="button" type="button" value="Search" onclick="search(field.value)">
+					<div id="divContent" class="shadowbox" style="visibility: hidden">	    
+						<h2 id=title >Content</h2>
+						<div class="hr"></div>
+						<div id=author></div>
+						<div id=date></div>
+						<div class="hr"></div>
+						<div id=content></div>
+					</div>
+				</form>
+			</div>
+			<div id="central">
+				<div id="result" class="shadowbox" style="visibility: hidden ">
+				</div>
+			</div>
         <?php include 'Footer.php' ?>
     </body>
 </html>
